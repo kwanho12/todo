0b9aeb5d7e0dc7f79caf9fafda005baa5f9ac8be
kwanho12 <jinkwanho14@naver.com>
2024-07-27 16:22:07 +0900

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    data() {
+        return {
+            inputMsg: '',
+        }
+    },
+    emits: ['addTodo'],
+    methods: {
+        addTodo() {
+            this.$emit('addTodo', this.inputMsg);
+            this.inputMsg = '';
+        }
+    }
+}
+</script>
+<template>
+    <div class="todo__input">
+        <input
+          v-model="inputMsg"
+          type="text"
+          class="todo__input-text"
+          placeholder="할 일을 입력하세요."
+          @keydown.enter="addTodo" />
+        <button class="todo__input-btn" @click="addTodo">등록</button>
+      </div>
+</template>
